bbecc54d021eb559c46ec264de242849a964b387
Neha <67725607+nehasoni05@users.noreply.github.com>
2020-12-13 13:41:34 +0530

    Create Solution.java

---
+++ Java/Introduction/Java Loops/Solution.java
@@ -0,0 +1,48 @@
+/*
+Task
+Given an integer N , print its first 10 multiples.
+
+Sample Input
+
+2
+Sample Output
+
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+
+SOLTUTION->
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(int i=1;i<=10;i++)
+        {
+            int out=N*i;
+            System.out.println(N+" x "+i+" = "+out);
+        }
+        scanner.close();
+    }
+}
