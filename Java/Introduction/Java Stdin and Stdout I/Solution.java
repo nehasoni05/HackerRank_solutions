f360373448c6808c9bf1d44e0015c054eb3522b3
Neha <67725607+nehasoni05@users.noreply.github.com>
2020-12-13 13:24:14 +0530

    Update Solution.java

--- Java/Introduction/Java Stdin and Stdout I/Solution.java
+++ Java/Introduction/Java Stdin and Stdout I/Solution.java
@@ -1,3 +1,25 @@
+/*
+Task
+In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+Input Format
+
+There are  lines of input, and each line contains a single integer.
+
+Sample Input
+
+42
+100
+125
+Sample Output
+
+42
+100
+125
+
+SOLUTION->
+*/
+
 import java.util.*;
 
 public class Solution {
