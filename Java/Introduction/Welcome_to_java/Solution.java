35153c469ce601fafa8a4c40fdacfe4e6b4870b5
Neha <67725607+nehasoni05@users.noreply.github.com>
2020-12-13 13:25:20 +0530

    Update Solution.java

--- Java/Introduction/Welcome_to_java/Solution.java
+++ Java/Introduction/Welcome_to_java/Solution.java
@@ -1,3 +1,17 @@
+/*
+Input Format
+
+There is no input for this challenge.
+
+Output Format
+
+You must print two lines of output:
+Print Hello, World. on the first line.
+Print Hello, Java. on the second line
+
+SOLUTION->
+*/
+
 public class Solution 
 {
     public static void main(String[] args) {
