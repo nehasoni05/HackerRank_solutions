4742a42c8dcc8abfaa495e52a9562eb040d69d68
Neha <67725607+nehasoni05@users.noreply.github.com>
2020-12-15 19:41:32 +0530

    Create Solution.java

---
+++ Java/Data Structures/Java 2D Array/Solution.java
@@ -0,0 +1,92 @@
+/* Java 2D Array
+You are given a 6∗6 2D array. An hourglass in an array is a portion shaped like this:
+a b c
+  d
+e f g
+For example, if we create an hourglass using the number 1 within an array full of zeros, it may look like this:
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+Actually there are many hourglasses in the array above. The three leftmost hourglasses are the following:
+1 1 1     1 1 0     1 0 0
+  1         0         0
+1 1 1     1 1 0     1 0 0
+
+The sum of an hourglass is the sum of all the numbers within it. The sum for the hourglasses above are 7, 4, and 2, respectively.
+In this problem you have to print the largest sum among all the hourglasses in the array.
+
+Input Format
+There will be exactly 6 lines, each containing 6 integers seperated by spaces. Each integer will be between −9 and 9 inclusive.
+
+Output Format
+Print the answer to this problem on a single line.
+
+Sample Input
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+Sample Output
+19 
+
+SOLUTION->
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        scanner.close();
+        System.out.println(maxHourglass(arr));
+    }
+    
+    public static int maxHourglass(int [][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+                int sum = findSum(arr, row, col);
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+    
+    private static int findSum(int [][] arr, int r, int c) {
+        int sum = arr[r+0][c+0] + arr[r+0][c+1] + arr[r+0][c+2]
+                                + arr[r+1][c+1] + 
+                  arr[r+2][c+0] + arr[r+2][c+1] + arr[r+2][c+2];
+        return sum;
+    }
+}
+
+
+
