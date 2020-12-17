2f6a2c782750b1ee023281725e95a26752333f52
Neha <67725607+nehasoni05@users.noreply.github.com>
2020-12-17 19:21:59 +0530

    Update Solution.java

--- Java/Data Structures/Java Dequeue/Solution.java
+++ Java/Data Structures/Java Dequeue/Solution.java
@@ -21,35 +21,36 @@ Sample Output
 SOLUTION->
 */
 
-    import java.util.*;
-    public class test {
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            Deque deque = new ArrayDeque<>();
-            int n = in.nextInt();
-            int m = in.nextInt();           
-            int uniq=0;
-            for (int i = 0; i < n; i++)
+import java.util.*;
+
+public class test 
+{
+    public static void main(String[] args) 
+    {
+         Scanner in = new Scanner(System.in);
+         Deque<Integer> deque = new ArrayDeque<>();
+         HashSet<Integer> set = new HashSet<>();
+         int n = in.nextInt();
+         int m = in.nextInt();
+         int max = Integer.MIN_VALUE;
+
+         for (int i = 0; i < n; i++) 
+         {
+            int input = in.nextInt();
+            deque.add(input);
+            set.add(input);
+            
+            if (deque.size() == m) 
             {
-                int num = in.nextInt();
-               if(i==0)
-               {
-                  deque.add(num);
-                  uniq++; 
-               }
-               else
-               {
-                   if(deque.size()==m)
-                   {
-                       deque.removeFirst();
-                   }
-                   else
-                   {
-                       if(!deque.contains(num) && uniq<m)
-                       uniq++;
-                   }
-               }
+                if (set.size() > max) 
+                max = set.size();
+                
+                int first = deque.remove();
+                
+                if (!deque.contains(first)) 
+                set.remove(first);
             }
-            System.out.print(uniq);
         }
+        System.out.println(max);
     }
+}
