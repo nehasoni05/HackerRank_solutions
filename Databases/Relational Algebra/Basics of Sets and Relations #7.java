304bb2ebafef735a9afc44263b0b14ca78154377
Neha <67725607+nehasoni05@users.noreply.github.com>
2021-02-05 22:10:04 +0530

    Create Basics of Sets and Relations #7.java

---
+++ Databases/Relational Algebra/Basics of Sets and Relations #7.java
@@ -0,0 +1,22 @@
+/*
+Consider the following data table named Student.
+
+Student Name        Number  Sex  
+Nina                3412    F 
+Mike                1234    M  
+Nelson              2341    F  
+Here is another data table named Teaching Assistants
+
+Subject     ID
+Physics     3412
+Chemistry   1111
+Mathematics 2341  
+What is the count of rows returned in the following join operation?
+Student ⊳⊲(Number=ID) Teaching Assistants
+
+Only enter a single integer. Do not include any extra spaces or newlines.
+*/
+
+2
+
+/*We will join the rows in such a way that the Number attribute in Student table has the same value as the ID attribute in Teaching Assistants table. */
