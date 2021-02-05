a5ea8dbf23211185e8acedfb387af78925d5cf6c
Neha <67725607+nehasoni05@users.noreply.github.com>
2021-02-05 21:48:42 +0530

    Create Basics of Sets and Relations.java

---
+++ Databases/Relational Algebra/Basics of Sets and Relations.java
@@ -0,0 +1,14 @@
+/*
+You are given two sets.
+Set A = {1,2,3,4,5,6}
+Set B = {2,3,4,5,6,7,8}
+
+How many elements are present in AUB?
+Only enter the correct integer in the editor below. Do not include any extra spaces, tabs or newlines.
+
+Solution-
+*/
+
+8
+
+/* AUB={1,2,3,4,5,6,7,8}*/
