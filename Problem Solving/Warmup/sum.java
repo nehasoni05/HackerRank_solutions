2d3deedc3a62582839e9d9179d6d52733136414f
Neha Soni <67725607+nehasoni05@users.noreply.github.com>
2021-02-20 19:40:24 +0530

    Create sum.java

---
+++ Problem Solving/Warmup/sum.java
@@ -0,0 +1,34 @@
+/*
+Sample Input
+2        //a
+3        //b  
+
+Sample Output
+5
+
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class sum
+{
+    static int solveMeFirst(int a, int b) 
+    {
+      return a+b;    
+	  }
+
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+	}
+}
