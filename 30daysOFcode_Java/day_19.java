0386003dbf7fe37f3c83be3aa3218aedff018f68
Neha Soni <67725607+nehasoni05@users.noreply.github.com>
2021-03-12 11:18:10 +0530

    Create day_19.java

---
+++ 30daysOFcode_Java/day_19.java
@@ -0,0 +1,47 @@
+/*
+Task
+The AdvancedArithmetic interface and the method declaration for the abstract divisorSum(n) method are provided for you in the editor below.
+
+Complete the implementation of Calculator class, which implements the AdvancedArithmetic interface. The implementation for the divisorSum(n) method must return the sum of all 
+divisors of n.
+
+Sample Input
+6
+
+Sample Output
+I implemented: AdvancedArithmetic
+12
+*/
+
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) 
+    {
+        int sum=0,i;
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            sum+=i;
+        }
+        return sum;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
